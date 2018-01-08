ba6d3d784ca35939dca0efff9b1d01ab6c5b8e57
SilvaLu <spvv2706@gmail.com>
2018-01-08 14:47:42 -0500

    iniciando

---
+++ README.md
@@ -0,0 +1,5 @@
+DOM
+paso por 
+Loading,
+Interactve-parse (lectura de los )
+complete--> ultimo pas enla cual ya esta linkeado(cargado) todo
---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	setTimeout(function(){
+	window.location.href='views/register.html';
+	},3000);
+	// console.log(window.location.href);
+
+})
+
+
+
+
+
+
+
+
+	// alert('jo jo jo')
+	// var LIMITE = 140; variable constante , no debe cambiar.
+	// var $title = $('h1');
+	//accediendo  los elemtnos con jquery
+	// var $paragraph = $('h1');
+	// console.log
